c5df7a8d30be95e1b53f60b16fbb8706f0963536
LucasMatheus12 <matheusalexandre012@gmail.com>
2022-03-21 00:29:07 -0300

    adição de java script e alguns efeitos no cabeçalho

---
+++ projeto.js
@@ -0,0 +1,21 @@
+let contat; 
+let yCont; 
+
+window.addEventListener("load",function(){
+    contat = document.querySelector(".contato"); 
+    yCont = contat.offsetTop; 
+    yCont -= 50; 
+});
+
+
+
+window,addEventListener("scroll", function(){
+
+    let navBar = document.querySelector(".cabecalho__nav-bar"); 
+    navBar.classList.toggle("indica_quem-somos", window.scrollY > 550 && window.scrollY < 1200);
+    
+    navBar.classList.toggle("indica_servicos", window.scrollY > 1200 && window.scrollY < 1800); 
+
+    navBar.classList.toggle("indica_contato", window.scrollY > 1200 && window.scrollY < yCont);
+
+});
