c3df41f83f434499f71c616b28854ff634e87a64
LucasMatheus12 <matheusalexandre012@gmail.com>
2022-03-22 17:16:05 -0300

    Identação de código e animação no botão de enviar o formulário

--- projeto.js
+++ projeto.js
@@ -24,18 +24,18 @@ window.addEventListener("scroll", function () {
 
 
 const botao = document.getElementById("botao__enviar");
-const botao_ok = document.getElementById("botao_ok"); 
+const botao_ok = document.getElementById("botao_ok");
 
 // -----------------------FORMULÁRIO VERIFICA---------------------------
 botao.addEventListener('click', event => {
     event.preventDefault();
     let mensagem = document.getElementById("mensagem").value;
     let email = document.getElementById("email").value;
-    
+
     let quantidade = 0;
-    let qtd_aroba = 0; 
-    let qtd_elementos= 0; 
-    
+    let qtd_aroba = 0;
+    let qtd_elementos = 0;
+
     //percorrendo o email. 
     for (let i = 0; i < email.length; i++) {
         if (email[i] === "@") {
@@ -43,84 +43,100 @@ botao.addEventListener('click', event => {
         }
     }
 
-    
+
     // ----------------VERIFICAÇÃO-----------------------------------
     const qtd_Caracter = 33;
     const ponto = ".";
-    const arroba = "@"
-    const espaco = " "
+    const arroba = "@";
+    const espaco = " ";
     const qtd_caracter_dom = 16;
-    const elementos = "*&¨%$#!-+§{}[]?;><,'"+ '"^~'; 
+    const elementos = "*&¨%$#!-+§{}[]?;><,'" + '"^~';
 
 
     // -------------conta quantos arroba tem-----------
-    for(let i = 0; i < email.length; i++){
-        if(email[i] === arroba){
-            qtd_aroba++; 
+    for (let i = 0; i < email.length; i++) {
+        if (email[i] === arroba) {
+            qtd_aroba++;
         }
     }
 
     //--------------- compara com elementos de caracteres especiais -----------
 
-    for(let i = 0; i < email.length; i++){
-        for(let j= 0; j< elementos.length;j++){
-            if(email[i] === elementos[j]){
+    for (let i = 0; i < email.length; i++) {
+        for (let j = 0; j < elementos.length; j++) {
+            if (email[i] === elementos[j]) {
                 qtd_elementos++;
             }
         }
     }
 
-    let contador = 0; 
+    let contador = 0;
 
     if (email.includes(arroba) &&      //verifica o @ 
         (email.indexOf(arroba) < qtd_Caracter) && //verifica a quantidade de letras
         (email.includes(ponto)) &&  // verifica se contem o ponto 
         (!email.includes(espaco)) && // verifica se tem espaço no email 
-        ((email.indexOf(ponto) - 1 - email.indexOf(arroba) - 1) < qtd_caracter_dom)&&
-        (qtd_aroba === 1)&&
-        (qtd_elementos === 0)
-    ){
-        contador+=1
-    }else{
+        ((email.indexOf(ponto) - 1 - email.indexOf(arroba) - 1) < qtd_caracter_dom) && // verifica se no dominio tem mais de 16 caracteres
+        (qtd_aroba === 1) && //verifica quantidade de arroba no email 
+        (qtd_elementos === 0) && // verifica se tem caractere especial que tem no email 
+        (email[email.length - 1] !== ponto) //verifica se tem ponto no final do email. 
+    ) {
+        contador += 1
+
+    } else {
+        //exibe a mensagem: email inválido se para nas condiões de verificação. 
+
         const mensagem_error = "Email inválido";
         document.getElementById("mensagem_enviar").innerHTML = mensagem_error;
         document.getElementById("mensagem_enviar").classList.remove("verde");
         document.getElementById("mensagem_enviar").classList.add("vermelho");
-        
-        document.getElementById("botao_ok").classList.remove("desaparece"); 
-        document.getElementById("botao_ok").classList.add("aparece"); 
+
+        document.getElementById("botao_ok").classList.remove("desaparece");
+        document.getElementById("botao_ok").classList.add("aparece");
     }
+
     if (mensagem == false) {
+        //exibe a mensagem "erro no envio: insira uma mensagem", caso a condição de não ter nada no campo da mensagem. 
+
         document.getElementById("mensagem").classList.add("env");
         const mensagem = "Erro no envio: Insira uma mensagem";
         document.getElementById("mensagem_enviar").innerHTML = mensagem;
         document.getElementById("mensagem_enviar").classList.remove("verde");
         document.getElementById("mensagem_enviar").classList.add("vermelho");
 
-        document.getElementById("botao_ok").classList.remove("desaparece"); 
-        document.getElementById("botao_ok").classList.add("aparece"); 
+        document.getElementById("botao_ok").classList.remove("desaparece");
+        document.getElementById("botao_ok").classList.add("aparece");
     } else {
-        contador += 1; 
-    }   
-    console.log(contador); 
-    if(contador === 2){
+
+        contador += 1;
+    }
+
+    console.log(contador);
+
+    if (contador === 2) {
+
+        //se todas as condições forem válidaas exibe a mensagem: "Obrigado pelo o contato"
+
         const usuario = email.substr(0, email.indexOf(arroba));
         const mensagem_valida = `Obrigado pelo contato, ${usuario}!`
         document.getElementById("mensagem_enviar").innerHTML = mensagem_valida;
         document.getElementById("mensagem_enviar").classList.remove("vermelho");
         document.getElementById("mensagem_enviar").classList.add("verde");
 
-        document.getElementById("botao_ok").classList.remove("desaparece"); 
+        document.getElementById("botao_ok").classList.remove("desaparece");
         document.getElementById("botao_ok").classList.add("aparece");
-        
+
     }
 })
 
 botao_ok.addEventListener('click', event => {
-    event.preventDefault(); 
 
-    document.getElementById("mensagem").value = ""; 
-    document.getElementById("mensagem_enviar").innerHTML = ""; 
-    document.getElementById("botao_ok").classList.remove("aparece"); 
-    document.getElementById("botao_ok").classList.add("desaparece");    
-    })
+    //função anônima para a criação do botão "ok" que exibe ao lado da mensagem de erro ou de acerto.  
+
+    event.preventDefault();
+
+    document.getElementById("mensagem").value = "";
+    document.getElementById("mensagem_enviar").innerHTML = "";
+    document.getElementById("botao_ok").classList.remove("aparece");
+    document.getElementById("botao_ok").classList.add("desaparece");
+})
