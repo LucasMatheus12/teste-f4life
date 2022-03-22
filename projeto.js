ee107b34040525d2e321c832d18d49e9ca4e92e6
LucasMatheus12 <matheusalexandre012@gmail.com>
2022-03-22 15:04:43 -0300

    aicionando a verificação do formulário

--- projeto.js
+++ projeto.js
@@ -9,7 +9,6 @@ window.addEventListener("load", function () {
 });
 
 
-
 window.addEventListener("scroll", function () {
 
     let navBar = document.querySelector(".cabecalho__nav-bar");
@@ -21,3 +20,82 @@ window.addEventListener("scroll", function () {
 });
 
 // -------------------------- FORMULÁRIO ----------------------
+
+
+const botao = document.getElementById("botao__enviar");
+const botao_ok = document.getElementById("botao_ok"); 
+
+// -----------------------FORMULÁRIO VERIFICA---------------------------
+botao.addEventListener('click', event => {
+    event.preventDefault();
+    let mensagem = document.getElementById("mensagem").value;
+    let email = document.getElementById("email").value;
+    
+    let quantidade = 0;
+
+    //percorrendo o email. 
+    for (let i = 0; i < email.length; i++) {
+        if (email[i] === "@") {
+            quantidade++;
+        }
+    }
+    // ----------------VERIFICAÇÃO-----------------------------------
+    const qtd_Caracter = 33;
+    const ponto = ".";
+    const arroba = "@"
+    const espaco = " "
+    const qtd_caracter_dom = 16;
+
+     
+    let contador = 0; 
+
+    if (email.includes(arroba) &&      //verifica o @ 
+        (email.indexOf(arroba) < qtd_Caracter) && //verifica a quantidade de letras
+        (email.includes(ponto)) &&  // verifica se contem o ponto 
+        (!email.includes(espaco)) && // verifica se tem espaço no email 
+        ((email.indexOf(ponto) - 1 - email.indexOf(arroba) - 1) < qtd_caracter_dom)
+    ){
+        contador+=1
+    }else{
+        const mensagem_error = "Email inválido";
+        document.getElementById("mensagem_enviar").innerHTML = mensagem_error;
+        document.getElementById("mensagem_enviar").classList.remove("verde");
+        document.getElementById("mensagem_enviar").classList.add("vermelho");
+        
+        document.getElementById("botao_ok").classList.remove("desaparece"); 
+        document.getElementById("botao_ok").classList.add("aparece"); 
+    }
+    if (mensagem == false) {
+        document.getElementById("mensagem").classList.add("env");
+        const mensagem = "Erro no envio: Insira uma mensagem";
+        document.getElementById("mensagem_enviar").innerHTML = mensagem;
+        document.getElementById("mensagem_enviar").classList.remove("verde");
+        document.getElementById("mensagem_enviar").classList.add("vermelho");
+
+        document.getElementById("botao_ok").classList.remove("desaparece"); 
+        document.getElementById("botao_ok").classList.add("aparece"); 
+    } else {
+        contador += 1; 
+    }   
+    console.log(contador); 
+    if(contador === 2){
+        const usuario = email.substr(0, email.indexOf(arroba));
+        const mensagem_valida = `Obrigado pelo contato, ${usuario}!`
+        document.getElementById("mensagem_enviar").innerHTML = mensagem_valida;
+        document.getElementById("mensagem_enviar").classList.remove("vermelho");
+        document.getElementById("mensagem_enviar").classList.add("verde");
+
+        document.getElementById("botao_ok").classList.remove("desaparece"); 
+        document.getElementById("botao_ok").classList.add("aparece");
+        
+    }
+})
+
+botao_ok.addEventListener('click', event => {
+    event.preventDefault(); 
+
+    document.getElementById("mensagem").value = ""; 
+    document.getElementById("mensagem_enviar").innerHTML = ""; 
+    document.getElementById("botao_ok").classList.remove("aparece"); 
+    document.getElementById("botao_ok").classList.add("desaparece");    
+    })
