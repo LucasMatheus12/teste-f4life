de5d649e17e0204c71407c68ddc995c135a858e5
LucasMatheus12 <matheusalexandre012@gmail.com>
2022-03-21 23:20:49 -0300

    modificando css

--- projeto.js
+++ projeto.js
@@ -1,21 +1,23 @@
-let contat; 
-let yCont; 
+let contat;
+let yCont;
 
-window.addEventListener("load",function(){
-    contat = document.querySelector(".contato"); 
-    yCont = contat.offsetTop; 
-    yCont -= 50; 
+
+window.addEventListener("load", function () {
+    contat = document.querySelector(".contato");
+    yCont = contat.offsetTop;
+    yCont -= 50;
 });
 
 
 
-window,addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
 
-    let navBar = document.querySelector(".cabecalho__nav-bar"); 
+    let navBar = document.querySelector(".cabecalho__nav-bar");
     navBar.classList.toggle("indica_quem-somos", window.scrollY > 550 && window.scrollY < 1200);
-    
-    navBar.classList.toggle("indica_servicos", window.scrollY > 1200 && window.scrollY < 1800); 
 
-    navBar.classList.toggle("indica_contato", window.scrollY > 1200 && window.scrollY < yCont);
+    navBar.classList.toggle("indica_servicos", window.scrollY > 1200 && window.scrollY < 1800);
 
+    navBar.classList.toggle("indica_contato", window.scrollY > 1800 && window.scrollY < yCont);
 });
+
+// -------------------------- FORMULÁRIO ----------------------
